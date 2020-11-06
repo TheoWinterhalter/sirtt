939cddcdfe47d843a2c00207123d63104e0c0446
Théo Winterhalter <isheeft@gmail.com>
2020-11-06 15:29:41 +0100

    Define substitution

--- theories/SSubst.v
+++ theories/SSubst.v
@@ -3,6 +3,8 @@
 From Coq Require Import Utf8 List Nat.
 Require Import Util SAst.
 
+Import ListNotations.
+
 Fixpoint lift n k t : term :=
   match t with
   | var i => var (if Nat.leb k i then (n + i) else i)
@@ -28,3 +30,39 @@ Fixpoint lift n k t : term :=
   end.
 
 Notation lift0 n := (lift n 0).
+
+Fixpoint subst s k u :=
+  match u with
+  | var n =>
+    if Nat.leb k n then
+      match nth_error s (n - k) with
+      | Some b => lift0 k b
+      | None => var (n - List.length s)
+      end
+    else var n
+  | lam l A t => lam l (subst s k A) (subst s (S k) t)
+  | app l u v => app l (subst s k u) (subst s k v)
+  | Prod l A B => Prod l (subst s k A) (subst s (S k) B)
+  | ex u p => ex (subst s k u) (subst s k p)
+  | wit t => wit (subst s k t)
+  | prf t => prf (subst s k t)
+  | Sum A P => Sum (subst s k A) (subst s (S k) P)
+  | zero => zero
+  | succ t => succ (subst s k t)
+  | elim_nat P z sc t =>
+    elim_nat (subst s k P) (subst s k z) (subst s k sc) (subst s k t)
+  | Nat => Nat
+  | vnil A => vnil (subst s k A)
+  | vcons A a m v => vcons (subst s k A) (subst s k a) (subst s k m) (subst s k v)
+  | elim_vec A P e c m v =>
+    elim_vec (subst s k A) (subst s k P)
+             (subst s k e) (subst s k c) (subst s k m) (subst s k v)
+  | vec A m => vec (subst s k A) (subst s k m)
+  | univ s => univ s
+  end.
+
+(** Substitutes [t1 ; .. ; tn] in u for [Rel 0; .. Rel (n-1)] *in parallel* *)
+Notation subst0 t := (subst t 0).
+Definition subst1 t k u := subst [t] k u.
+Notation subst10 t := (subst1 t 0).
+Notation "M { j := N }" := (subst1 N j M) (at level 10, right associativity).
