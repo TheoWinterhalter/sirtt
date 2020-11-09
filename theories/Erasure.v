1c1dd4e2ccf21e2370a1afbd2d35b820d7da72a1
Théo Winterhalter <isheeft@gmail.com>
2020-11-09 13:52:01 +0100

    Translation is buggy

--- theories/Erasure.v
+++ theories/Erasure.v
@@ -1,6 +1,6 @@
 (* Erasure translation from SIRTT to MLTT *)
 
-From Coq Require Import Utf8.
+From Coq Require Import Utf8 List Nat.
 Require Import Util SIRTT MLTT.
 
 Definition dummy : MLTT.term := MLTT.var 0.
@@ -31,6 +31,30 @@ Fixpoint trans (t : SIRTT.term) : MLTT.term :=
   end.
 
 (* Some properties about the translation itself *)
+
+Lemma erase_subst :
+  ∀ σ k t,
+    trans (SIRTT.subst σ k t) = MLTT.subst (map trans σ) k (trans t).
+Proof.
+  intros σ k t.
+  induction t in σ, k |- *.
+  - cbn. destruct (PeanoNat.Nat.leb_spec k n).
+    + rewrite nth_error_map. destruct nth_error eqn:e.
+      * cbn. admit.
+      * cbn. rewrite map_length. reflexivity.
+    + reflexivity.
+  - destruct l.
+    + cbn. rewrite ?IHt1, ?IHt2. reflexivity.
+    (* + cbn. rewrite ?IHt1, ?IHt2. reflexivity.
+    + *)
+    (* The translation of variables is most likely wrong.
+      It is much more complicated because we remove binders.
+      The translation operation should probably take (at least) an integer
+      to offset things. But it basically has to do strengthening which is not
+      that easy… It feels like it should take a list of the forgotten variables.
+      This is not so great, an alternative would be welcome.
+    *)
+Abort.
 
 Lemma erase_topred_term :
   ∀ u v σ,
