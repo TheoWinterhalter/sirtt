4e8f9cc83d311502762a147c138ed23fb8c02a13
Théo Winterhalter <isheeft@gmail.com>
2020-11-09 21:56:07 +0100

    Define max on levels

--- theories/Level.v
+++ theories/Level.v
@@ -26,3 +26,41 @@ where "u ⊏ v" := (more_relevant u v).
 
 Definition potentially_more_relevant := clos_refl more_relevant.
 Notation "u ⊑ v" := (potentially_more_relevant u v) (at level 20).
+
+Definition max u v :=
+  match u, v with
+  | R, v => v
+  | S, R => S
+  | S, v => v
+  | I, v => I
+  end.
+
+Notation "u ⊔ v" := (max u v) (at level 19).
+
+Lemma max_sym :
+  ∀ u v,
+    max u v = max v u.
+Proof.
+  intros u v.
+  destruct u, v. all: reflexivity.
+Qed.
+
+Lemma max_le_l :
+  ∀ u v,
+    u ⊑ u ⊔ v.
+Proof.
+  intros u v.
+  destruct u, v. all: simpl.
+  all: try (left ; constructor).
+  all: try (right ; constructor).
+Qed.
+
+Lemma max_le_r :
+  ∀ u v,
+    v ⊑ u ⊔ v.
+Proof.
+  intros u v.
+  destruct u, v. all: simpl.
+  all: try (left ; constructor).
+  all: try (right ; constructor).
+Qed.
