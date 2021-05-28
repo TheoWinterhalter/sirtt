af0a4438f23fbf2958250519b8392a10235b1f1d
Théo Winterhalter <theo.winterhalter@gmail.com>
2021-05-28 12:44:15 +0200

    Progress with scoping SR

--- theories/SSR.v
+++ theories/SSR.v
@@ -18,18 +18,42 @@ Proof.
   intros Γ u v hs hr.
   induction hr.
   all: try solve [
+    scope_inv hs hs' ; intuition eauto
+  ].
+  all: try solve [
     scope_inv hs hs' ; constructor ; intuition eauto
   ].
-  - (* NEED the subst_scoping and so on, so first clean Erasure. *)
-    admit.
-  - (* NEED reveal_scope_sound *)
-    admit.
-  - admit.
-  - admit.
-  - admit.
-  - admit.
+  - scope_inv hs hs'. simpl in hs'. destruct hs' as [hv hu].
+    eapply subst_scoping with (Ξ := []).
+    2:{ constructor. 2: constructor. eauto. }
+    simpl.
+    eapply scoping_reveal_subst_k with (Δ := [ _ ]) in hv as h.
+    + erewrite e in h. simpl in h. eapply h.
+    + simpl. eapply reveal_scope_sound in hv as h.
+      rewrite e in h. scope_inv h h'.
+      intuition eauto.
+  - scope_inv hs hs'. destruct hs' as [hP [hz [hsc ht]]].
+    simpl. constructor. 2: simpl. all: constructor.
+    all: intuition eauto. 1: simpl.
+    all: eapply reveal_scope_sound in ht as h.
+    all: rewrite e in h.
+    all: scope_inv h h'.
+    all: eapply scoping_reveal_subst in ht as h2. 2,4: eauto.
+    all: rewrite e in h2. all: auto.
+  - scope_inv hs hs'. destruct hs' as [hA [hP [he [hc [hn ht]]]]].
+    simpl. constructor. 2: simpl. all: constructor. 2: simpl.
+    1: constructor. 2: simpl. 1: constructor. 2: simpl.
+    all: try solve [ intuition eauto ].
+    all: eapply reveal_scope_sound in ht as hs'.
+    all: rewrite e0 in hs'.
+    all: scope_inv hs' hs''. all: intuition auto.
+    all: eapply scoping_reveal_subst in ht as h ; [| shelve].
+    all: rewrite e0 in h. all: try eassumption.
+    (* Do we have a scoping_reveal_subst with ℓ instead of R? *)
+    all: admit.
+    Unshelve. all: eauto.
   - scope_inv hs hs'. constructor. 2: intuition eauto.
-    (* NEED scoping_psc *)
-    (* TODO Add to all try *)
+    (* We need to strengthen the IH *)
     admit.
+  - admit.
 Abort.
